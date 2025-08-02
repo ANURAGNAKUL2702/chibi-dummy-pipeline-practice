42a8df846fd2015da16798a0298e5bfdfce954b8
B. KANTE VENKATA ANURAG <anuragnakul2702@gmail.com>
2025-08-02 23:43:10 +0530

    Create lint-dummy.js

---
+++ frontend/lint-dummy.js
@@ -0,0 +1,13 @@
+var msg='This is a test'   // Bad spacing, no semicolon
+function greet(name){
+console.log('Hello '+name)  // Indentation, spacing issue
+}
+greet("Anurag")
+
+let nums=[1,2,3,4,5]
+nums.map(n=>{
+return n*2
+})
+
+
+
